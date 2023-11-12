bae0c26107258bd9ffb7e9bc123a7b368110f0e6
mtomasic2 <mtomasic20@student.foi.hr>
2023-11-12 22:05:03 +0100

    TIDIJ-61 Refactor code for better organization and readability

--- agent.py
+++ agent.py
@@ -1,43 +1,53 @@
 from flask import Flask
 from flask_sock import Sock
-import sys, time, json
-
+import sys
+import time
+import json
 import eventlet
 from eventlet import wsgi
 
 app = Flask(__name__)
 sock = Sock(app)
 
-@sock.route('/route-simulation')
-def car_simulation(ws):
-    start_point = {"latitude": 37.7749, "longitude": -122.4194}
-    end_point = {"latitude": 37.7749, "longitude": -122.4313}
+# Configuration
+SERVER_ADDRESS = '127.0.0.1'
+SERVER_PORT = 41414
 
+CAR_SIMULATION_ROUTE = '/route-simulation'
+ECHO_ROUTE = '/echo'
+
+# Constants for car simulation
+START_POINT = {"latitude": 37.7749, "longitude": -122.4194}
+END_POINT = {"latitude": 37.7749, "longitude": -122.4313}
+
+@sock.route(CAR_SIMULATION_ROUTE)
+def car_simulation(ws):
     for i in range(11):
-        progress = i / 10.0  # Računanje napretka
+        progress = i / 10.0
         current_location = {
-            "latitude": start_point["latitude"] + progress * (end_point["latitude"] - start_point["latitude"]),
-            "longitude": start_point["longitude"] + progress * (end_point["longitude"] - start_point["longitude"]),
+            "latitude": START_POINT["latitude"] + progress * (END_POINT["latitude"] - START_POINT["latitude"]),
+            "longitude": START_POINT["longitude"] + progress * (END_POINT["longitude"] - START_POINT["longitude"]),
         }
         ws.send(json.dumps(current_location))
         time.sleep(1)
 
-@sock.route('/echo')
+@sock.route(ECHO_ROUTE)
 def echo_socket(ws):
-    print("Klijent povezan")
+    print("Client connected")
     while True:
         message = ws.receive()
         if message is not None:
-            print("Primljena poruka od klijenta:", message)
-            response_message = f"Odgovor na: {message} i server saljeee pozz"
+            print("Received message from client:", message)
+            response_message = f"Response to: {message} and the server sends greetings"
             ws.send(response_message)
 
-while True:
+def run_server():
     try:
         if __name__ == '__main__':
-            wsgi.server(eventlet.listen(('127.0.0.1', 41414)), app)
-        pass
+            wsgi.server(eventlet.listen((SERVER_ADDRESS, SERVER_PORT)), app)
     except KeyboardInterrupt:
-        print('\nSkripta je zaustavljena.')
+        print('\nScript stopped.')
         sys.exit(0)
 
+if __name__ == '__main__':
+    run_server()
