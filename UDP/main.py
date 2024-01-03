f5efbf7c37a9624917a0ca848ee498e42e2c0c44
mtomasic2 <mtomasic20@student.foi.hr>
2024-01-03 18:13:35 +0100

    TIDIJ-62 Added exception logging

--- UDP/main.py
+++ UDP/main.py
@@ -1,6 +1,23 @@
 import sys
 import threading
 from entities.vehicle_manager import VehicleManager
+import atexit
+import traceback
+from datetime import datetime
+
+
+def cleanup():
+    print("Ovo će se izvršiti pri izlasku iz programa.")
+
+
+atexit.register(cleanup)
+
+
+def log_exception():
+    exception_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open("exceptions.log", "a") as log_file:
+        log_file.write(f"Exception occurred at: {exception_time}\n")
+        traceback.print_exc(file=log_file)
 
 
 if __name__ == "__main__":
@@ -11,5 +28,12 @@ if __name__ == "__main__":
     command_thread = threading.Thread(target=manager.receive_commands)
     command_thread.start()
 
-    while True:
-        pass
+    try:
+        while True:
+            pass
+    except Exception as e:
+        log_exception()
+        print(f"Izlaz iz programa zbog iznimke: {e}")
+    except KeyboardInterrupt:
+        log_exception()
+        print("Izlaz iz programa zbog pritiska tipke za prekid.")
